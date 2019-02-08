f8d5c6ac62b986235687d646ff4dbaa647aa5c7c
mrdaw93 <47173754+mrdaw93@users.noreply.github.com>
2019-02-08 11:10:18 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '10.130.125.18');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'pedidos');
